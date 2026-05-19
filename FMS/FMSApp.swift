e576f3ea31949a19359725b82f5f038129313d41
Sarthak Singh <sarthaksingh@Sarthaks-MacBook-Air.local>
2026-05-19 22:49:43 +0530

    Initial Setup

---
+++ FMS/FMSApp.swift
@@ -0,0 +1,18 @@
+//
+//  FMSApp.swift
+//  FMS
+//
+//  Created by Sarthak Singh on 19/05/26.
+//
+
+import SwiftUI
+
+@main
+@available(iOS 26.0, *)
+struct FMSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
